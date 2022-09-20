3ef3bd36bdc4e46c72dcc4ae56905f881424b635
adityackr <aditya09me@gmail.com>
2022-09-20 16:49:47 +0600

    merge: frontend and backend in one directory

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+const Note = require('./models/note');
 
 app.use(express.json());
+app.use(express.static('build'));
+app.use(cors());
 
 let notes = [
 	{
@@ -24,48 +30,34 @@ let notes = [
 	},
 ];
 
-const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
-};
-
 app.get('/', (req, res) => {
 	res.send(`<h1>Hello World!</h1>`);
 });
 
 app.get('/api/notes', (req, res) => {
-	res.json(notes);
+	Note.find({}).then((notes) => res.json(notes));
 });
 
 app.post('/api/notes', (req, res) => {
 	const body = req.body;
 
-	if (!body.content) {
+	if (body.content === undefined) {
 		return res.status(400).json({
 			error: 'content missing',
 		});
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
 		date: new Date(),
-		id: generateId(),
-	};
+	});
 
-	notes = [...notes, note];
-	res.json(note);
+	note.save().then((savedNote) => res.json(savedNote));
 });
 
 app.get('/api/notes/:id', (req, res) => {
-	const id = Number(req.params.id);
-	const note = notes.find((note) => note.id === id);
-
-	if (note) {
-		res.json(note);
-	} else {
-		res.status(404).end();
-	}
+	Note.findById(req.params.id).then((note) => res.json(note));
 });
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -75,7 +67,7 @@ app.delete('/api/notes/:id', (req, res) => {
 	res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,16 +1,10 @@
 import axios from 'axios';
 
-const basUrl = 'http://localhost:3001/notes';
+const basUrl = '/api/notes';
 
 const getAll = () => {
 	const request = axios.get(basUrl);
-	const nonExisting = {
-		id: 10000,
-		content: 'This is not saved to server',
-		date: '2022-09-12T10:40:33.552Z',
-		important: true,
-	};
-	return request.then((response) => [...response.data, nonExisting]);
+	return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
